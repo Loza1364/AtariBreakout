2916b82f31654849fe11f1a55513b0f30f850c66
Joel <719325@student.dusd.net>
2025-09-23 14:54:04 -0700

    playerMovementSetup

---
+++ Assets/Scripts/ballScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+
+    public float minY = -5.5f;
+    public float maxVeclocity = 15f;
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if(transform.position.y < minY)
+        {
+            transform.position = Vector3.zero;
+            rb.linearVelocity = Vector3.zero;
+        }
+
+        if(rb.linearVelocity.magnitude > maxVeclocity)
+        {
+            rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVeclocity);
+        }
+    }
+}
